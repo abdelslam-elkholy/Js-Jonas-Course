dd5391dbab5fa0c7807d370fa013573a2cb81a0f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-03 13:14:08 +0300

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,12 +2,12 @@
 
 let secretNumber =  Math.trunc(Math.random()*20) + 1;
 let score = 20;
-
 let heighScore =0;
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
-
+const numLabel = document.querySelector('.number');
 console.log(secretNumber);
+
 const guess = (message) =>
 {
     document.querySelector('.message').textContent = message;
@@ -22,6 +22,9 @@ const playGame = ()=>
     {
         document.querySelector('.highscore').textContent = score;
         document.querySelector('.message').textContent = 'You Got It';
+        document.querySelector('body').style.backgroundColor = 'green'
+        numLabel.textContent = secretNumber
+        numLabel.style.width = '30rem'
     }
     else if( num > secretNumber)
     {
