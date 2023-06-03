e7d7ce6736da0fa9adb2bd166d8dc6fc915d247a
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-03 12:35:56 +0300

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,8 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random()*20) + 1;
-const score = document.querySelector('.score').value;
+let secretNumber =  Math.trunc(Math.random()*20) + 1;
+let score = document.querySelector('.score').textContent;
+let heighScore = document.querySelector('.label-highscore').textContent
+let message = document.querySelector('.message').textContent;
+const checkBtn = document.querySelector('.check');
+
