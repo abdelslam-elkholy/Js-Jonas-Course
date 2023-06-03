9c3bb49b3dd7bee0913c2fbfad1c1e60a8dc7ed1
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-03 14:53:22 +0300

    update

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,28 +1,34 @@
 'use strict';
 
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let heighScore = 0;
-let message = document.querySelector('.message').textContent;
-let scoreLabel = document.querySelector('.score').textContent;
-let bodyColor = document.querySelector('body').style.backgroundColor;
+let message = document.querySelector('.message')
+let scoreLabel = document.querySelector('.score')
+let bodyColor = document.querySelector('body')
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
 const numLabel = document.querySelector('.number');
+
+
 console.log(secretNumber);
+
 
 const guess = (text)=>
 {
-    message = text;
+    console.log('hey im working from guess');
+    message.textContent = text;
     score --
-    scoreLabel = score;
+    scoreLabel.textContent = score;
 }
+
 
 const playGame = () => {
   const num = document.querySelector('.guess').value;
-
+  console.log('hey im working from play');
   if (num) {
-
+    console.log('hey im working from play if');
     if (num === secretNumber) {
       document.querySelector('.highscore').textContent = score;
       numLabel.textContent = secretNumber;
@@ -36,7 +42,8 @@ const playGame = () => {
   }
   else
   {
-    message= 'enter a Number'; 
+    console.log('hey im working from play else');
+    message.textContent= 'enter a Number'; 
   }
 };
 checkBtn.addEventListener('click', () => {
@@ -51,4 +58,8 @@ againBtn.addEventListener('click' , ()=>
 {
     score = 20;
     message = 'Start guessing...'
+    numLabel.textContent = '?'
+    numLabel.style.width = '15rem'
+    bodyColor = 'black'
+
 })
