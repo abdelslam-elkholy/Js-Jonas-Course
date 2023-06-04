f2f9b0415b90d0fb026b38352c291ff6798adb6f
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-06-04 14:59:48 +0300

    update

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,3 +3,4 @@
 const openModalBtn = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
